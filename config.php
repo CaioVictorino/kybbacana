99f4b901d5218b5cf55da3d870baaa3631892cbc
Caio Victorino <caiodeveloper@hotmail.com>
2024-04-10 16:38:04 -0300

    base

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("BASE_DIR", dirname(__FILE__));
+define("BASE_DIR", dirname(__DIR__));
 define("TITLE", "KybbaCana");
 define("ASSET", '\\public\\assets\\');
 define("STORAGE", '\\public\\storage\\');
